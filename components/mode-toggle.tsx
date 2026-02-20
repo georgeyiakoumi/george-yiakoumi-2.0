a15d95ae6cc55769d6e42bfa0c110d2d5815fa1c
George Yiakoumi <gykmi91@protonmail.com>
2026-02-20 13:09:21 +0000

    fix: adjust z-index and remove unnecessary rotation from Moon icon in ModeToggle component

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -19,14 +19,14 @@ export function ModeToggle() {
   }
 
   return (
-    <div className="fixed box-border flex items-center justify-center p-4 lg:p-0 right-4 top-4 lg:right-16 lg:top-16 z-10">
+    <div className="fixed box-border flex items-center justify-center p-4 lg:p-0 right-4 top-4 lg:right-16 lg:top-16 z-11">
       <Tooltip>
         <TooltipTrigger asChild>
           <Button variant="secondary" size="icon" onClick={toggleTheme} className="bg-background xl:cursor-pointer">
             <AnimateIcon animate={animate} asChild>
               <>
                 <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 transition-all dark:scale-100" />
               </>
             </AnimateIcon>
             <span className="sr-only">Toggle theme</span>
