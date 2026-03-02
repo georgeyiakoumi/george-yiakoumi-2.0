5b44abf437bee012d61a5a34c41d4243bbcb8432
George Yiakoumi <gykmi91@protonmail.com>
2026-03-02 14:22:36 +0000

    fix: revert to standard .next directory - Netlify plugin doesn't handle custom distDir properly

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // TEMPORARY: Change output directory to bypass Netlify cache
-  distDir: '.next-clean',
   images: {
     remotePatterns: [
       {
